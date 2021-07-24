f059c73c3575635b6b2a97a6ca6146beade7b9dd
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-07-24 18:26:05 +0545

    added another meetup data with form and used useRef, useLocation hooks

--- meetup-app/src/pages/NewMeetup.js
+++ meetup-app/src/pages/NewMeetup.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import AddMeetup from "../components/meetups/AddMeetup";
 
 const NewMeetup = () => {
+  const history = useHistory();
+
+  const addMeetupHandler = async (meetupData) => {
+    await fetch(
+      "https://react-getting-started-fc03c-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  };
+
   return (
-    <div>
-      <h1>Add Meetup page</h1>
-    </div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <AddMeetup onAddMeetup={addMeetupHandler} />
+    </section>
   );
 };
 
