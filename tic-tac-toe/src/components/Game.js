6092d131f53269d51b6d3d0838404dcb523e851b
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-08-20 13:25:30 +0545

    added time travel to tic tac toe

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -8,41 +8,49 @@ const styles = {
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  const [stepNumber, setStepNumber] = useState(0);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (index) => {
-    const boardCopy = [...board];
-    // if user click an occupied square or if game is won return
-    if (winner || boardCopy[index]) return;
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+    // if user click an occupied square or if game is won, return
+    if (winner || squares[index]) return;
     //Put an X or O in the clicked square
-    boardCopy[index] = xIsNext ? "X" : "O";
-    setBoard(boardCopy);
+    squares[index] = xIsNext ? "X" : "O";
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXIsNext(!xIsNext);
   };
 
-  const renderMoves = () => {
-    return (
-      <button
-        onClick={() => {
-          setBoard(Array(9).fill(null));
-        }}
-      >
-        Start Game
-      </button>
-    );
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
   };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move #${move}` : "Go to start";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
 
   return (
     <>
-      <Board squares={board} onClick={handleClick} />
+      <Board squares={history[stepNumber]} onClick={handleClick} />
       <div style={styles}>
         <p>
           {winner
             ? `Winner : ${winner}`
-            : `Next Palyer : ${xIsNext ? "X" : "O"}`}
+            : `Next Player : ${xIsNext ? "X" : "O"}`}
         </p>
+        <h3>History</h3>
         {renderMoves()}
       </div>
     </>
